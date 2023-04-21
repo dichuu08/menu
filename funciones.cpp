1b89d015e885fa358b568d7cf705d2f6011f1404
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-21 08:18:42 -0600

    Programa con menu

--- funciones.cpp
+++ funciones.cpp
@@ -1,16 +1,49 @@
 #include<iostream>
 using namespace std;
-#define MAX 10
-//Arreglo
+
+#define MAX 100
+
+// Arreglo
 int edades[10];
 int pos = 0; //posicion de elemento
 
 void agregarEdad(int edad);
 bool hayEspacio();
+void mostrarEdades();
+bool esMayor(int edad);
+
 void agregarEdad(int edad){
     edades[pos] = edad;
     pos++;
 }
 bool hayEspacio(){
     return pos < MAX;
+}
+void mostrarEdades(){
+    for (int i = 0; i < pos; i++)
+    {
+        cout<<edades[i]<<endl;
+    }
+}
+bool esMayor(int edad){
+    return edad>= 18;
+}
+void mostrarEdadesMayores(){
+    for (int i = 0; i < pos; i++)
+    {
+        if (esMayor(edades[i]))
+        {
+            cout<<edades[i]<<endl;
+        }
+    }
+}
+int menu(){
+    int op;
+    cout<<"Listado de edades\n";
+    cout<<"1. Agregar edad\n";
+    cout<<"2. Mostrar edades\n";
+    cout<<"3. Mostrar edades mayores o iguales a 18\n";
+    cout<<"4. Salir\n";
+    cin>>op;
+    return op;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include<iostream>
+#include "funciones.cpp"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int cant, edad, i = 0, op = 0;
+    do{
+        op = menu();
+        switch (op)
+        {
+        case 1:
+            cout<<"Ingrese la edad: ";
+            cin>>edad;
+            agregarEdad(edad);
+        case 2: 
+            mostrarEdades();
+            system("pause");
+            break;
+        case 3:
+            mostrarEdadesMayores();
+            system("pause");
+            break;
+        case 4: 
+        break;
+        
+        default:
+        cout<<"Opcion invalida...\n";
+        system("pause");
+            break;
+        }
+    }while(op != 4);
     return 0;
 }
+
